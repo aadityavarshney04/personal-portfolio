b69142fc5fea904695a77f1581b94d436a1caeda
Aaditya Varshney <aaditya.varshney04@gmail.com>
2026-01-31 12:33:08 +0530

    Add section IDs for navbar scrolling

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,28 +1,44 @@
-
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import HeroSection from '../components/HeroSection';
-import AboutSection from '../components/AboutSection';
-import SkillsSection from '../components/SkillsSection';
-import ProjectsSection from '../components/ProjectsSection';
-import EducationSection from '../components/EducationSection';
-import ContactSection from '../components/ContactSection';
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import SkillsSection from "../components/SkillsSection";
+import ProjectsSection from "../components/ProjectsSection";
+import EducationSection from "../components/EducationSection";
+import ContactSection from "../components/ContactSection";
 
 const Index = () => {
   return (
     <div className="flex flex-col min-h-screen bg-black text-white">
       <Header />
-      
+
       <main className="flex-grow">
-        <HeroSection />
-        <AboutSection />
-        <SkillsSection />
-        <ProjectsSection />
-        <EducationSection />
-        <ContactSection />
+        <section id="home">
+          <HeroSection />
+        </section>
+
+        <section id="about">
+          <AboutSection />
+        </section>
+
+        <section id="skills">
+          <SkillsSection />
+        </section>
+
+        <section id="projects">
+          <ProjectsSection />
+        </section>
+
+        <section id="education">
+          <EducationSection />
+        </section>
+
+        <section id="contact">
+          <ContactSection />
+        </section>
       </main>
-      
+
       <Footer />
     </div>
   );
